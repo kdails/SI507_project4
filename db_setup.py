df55a1bbb4c5cbb74af9a754e362d4bbe209df0a
kdails <35503089+kdails@users.noreply.github.com>
2019-04-03 09:04:57 -0400

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import select
 import json, csv
 # some of this code based on info from https://docs.sqlalchemy.org/en/latest/orm/collections.html ( specifically many to many relationships for stateparks with the same names)
 # set up base, session, and engine
+
 engine = create_engine('sqlite:///state_parks_info.sqlite', echo=False)
 
 Base = declarative_base()
@@ -16,28 +17,36 @@ Base.metadata.bind = engine
 session.configure(bind=engine)
 
 # define db function making
+
+
 def init_db():
     Base.metadata.create_all(engine)
     return engine
 
 # define table class
+
+
 class State(Base):
     __tablename__ = 'States'
     Id = Column(Integer, primary_key=True, autoincrement=True)
-    State = Column(String(24), nullable = False)
     Abbr = Column(String(2), nullable = False)
+    State = Column(String(24), nullable = False)
     URL = Column(String(250), nullable = False)
     Parks_Rel = relationship('Park',secondary='association',back_populates='State_Rel',lazy='dynamic')
 # define table class
+
+
 class Park(Base):
     __tablename__ = 'Parks'
     Id = Column(Integer, primary_key=True, autoincrement=True)
     Name = Column(String(250))
-    Type = Column(String(250))
     Descr = Column(String(2000))
     Location = Column(String(250))
+    Type = Column(String(250))
     State_Rel = relationship('State',secondary='association',back_populates='Parks_Rel',lazy='dynamic')
 # define table class
+
+
 class StateParkAssociation(Base):
     __tablename__ = 'association'
     State_Id = Column(Integer, ForeignKey('States.Id'),primary_key=True)
