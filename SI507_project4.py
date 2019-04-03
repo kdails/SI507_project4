99c1c51e507b586cdf1b74a4219bd5a5590d6aa5
Kara <karahdailey@gmail.com>
2019-04-02 22:39:17 -0400

    first commit

---
+++ SI507_project4.py
@@ -0,0 +1,4 @@
+#setting up file structure here
+from bs4 import BeautifulSoup
+import requests, json, csv
+from setup import *
---
+++ setup.py
@@ -0,0 +1,6 @@
+# setting up the class and database file here with required imports
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import *
+from sqlalchemy.sql import select
+import json, csv
